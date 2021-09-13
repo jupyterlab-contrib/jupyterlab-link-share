a9ab3dde03285a7b8597ceb1e26412bcf01bebca
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-09-13 17:48:43 +0200

    Adopt Jupyter Packaging 0.10 and the releaser (#16)
    * Adopt Jupyter Packaging 0.10 and the releaser
    
    * Fix ensured_targets
    
    * Fix style -> static
    
    * Add CHANGELOG.md
    
    * Add publishConfig
    
    * Add notebook as a dependency
    
    * Handle missing notebook dependency
    
    * Fix generator -> list

--- jupyterlab_link_share/handlers.py
+++ jupyterlab_link_share/handlers.py
@@ -3,15 +3,23 @@ import json
 import tornado
 
 from jupyter_server.base.handlers import APIHandler
-from jupyter_server.serverapp import list_running_servers as list_jupyter_servers
 from jupyter_server.utils import url_path_join
-from notebook.notebookapp import list_running_servers as list_notebook_servers
+from jupyter_server.serverapp import list_running_servers as list_jupyter_servers
+
+list_notebook_servers = None
+
+try:
+    from notebook.notebookapp import list_running_servers as list_notebook_servers
+except:
+    pass
 
 
 class RouteHandler(APIHandler):
     @tornado.web.authenticated
     def get(self):
-        servers = list(list_notebook_servers()) + list(list_jupyter_servers())
+        servers = list(list_jupyter_servers())
+        if list_notebook_servers:
+            servers += list(list_notebook_servers())
         # sort by pid so PID 1 is first in Docker and Binder
         servers.sort(key=lambda x: x["pid"])
         self.finish(json.dumps(servers))
--- setup.py
+++ setup.py
@@ -4,13 +4,6 @@ jupyterlab-link-share setup
 import json
 from pathlib import Path
 
-from jupyter_packaging import (
-    create_cmdclass,
-    install_npm,
-    ensure_targets,
-    combine_commands,
-    skip_if_exists,
-)
 import setuptools
 
 HERE = Path(__file__).parent.resolve()
@@ -19,15 +12,12 @@ HERE = Path(__file__).parent.resolve()
 name = "jupyterlab-link-share"
 package = name.replace('-', '_')
 
-# Get our version
-with (HERE / "package.json").open() as f:
-    version = json.load(f)["version"]
-
 lab_path = HERE / package / "labextension"
 
 # Representative files that should exist after a successful build
-jstargets = [
+ensured_targets = [
     str(lab_path / "package.json"),
+    str(lab_path / "static" / "style.js"),
 ]
 
 package_data_spec = {package: ["*"]}
@@ -49,54 +39,26 @@ data_files_spec = [
     ),
 ]
 
-cmdclass = create_cmdclass(
-    "jsdeps", package_data_spec=package_data_spec, data_files_spec=data_files_spec
-)
 
-js_command = combine_commands(
-    install_npm(HERE, build_cmd="build:prod", npm=["jlpm"]),
-    ensure_targets(jstargets),
-)
+# Get the package info from package.json
+pkg_json = json.loads((HERE / "package.json").read_bytes())
 
-is_repo = (HERE / ".git").exists()
-if is_repo:
-    cmdclass["jsdeps"] = js_command
-else:
-    cmdclass["jsdeps"] = skip_if_exists(jstargets, js_command)
+try:
+    from jupyter_packaging import wrap_installers, npm_builder, get_data_files
 
-long_description = (HERE / "README.md").read_text()
+    # In develop mode, just run yarn
+    builder = npm_builder(build_cmd="build", build_dir=lab_path, source_dir="src")
+    cmdclass = wrap_installers(post_develop=builder, ensured_targets=ensured_targets)
 
-setup_args = dict(
-    name=name,
-    version=version,
-    url="https://github.com/jupyterlab-contrib/jupyterlab-link-share",
-    author="Project Jupyter Contributors",
-    description="JupyterLab Extension to share the URL to a running Jupyter Server",
-    long_description=(HERE / "README.md").read_text(),
-    long_description_content_type="text/markdown",
-    cmdclass=cmdclass,
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "jupyterlab~=3.0"
-    ],
-    zip_safe=False,
-    include_package_data=True,
-    python_requires=">=3.6",
-    license="BSD-3-Clause",
-    platforms="Linux, Mac OS X, Windows",
-    keywords=["Jupyter", "JupyterLab", "JupyterLab3"],
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Framework :: Jupyter",
-    ],
-)
+    setup_args = dict(
+        cmdclass=cmdclass,
+        data_files=get_data_files(data_files_spec),
+    )
+except ImportError:
+    setup_args = dict()
 
+
+setup_args["version"] = pkg_json["version"]
 
 if __name__ == "__main__":
     setuptools.setup(**setup_args)
